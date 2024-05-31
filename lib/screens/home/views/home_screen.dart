153f249dbef6f8096a68484a2c189546ad6a38a5
Caro <carolinegyireh@gmail.com>
2024-05-31 21:34:59 +0200

    button nav bar

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kasuwa/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,58 +10,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
-        title: Row(
-          children: [
-            Image.asset(
-              'images/kasuwa_logo.png',
-              scale: 10,
-            ),
-            const SizedBox(
-              width: 1,
-            ),
-            const Text(
-              'asuwa',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 25,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          title: Row(
+            children: [
+              Image.asset(
+                'images/kasuwa_logo.png',
+                scale: 10,
               ),
+              const SizedBox(
+                width: 1,
+              ),
+              const Text(
+                'Kasuwa',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon:
+                  const Icon(Icons.shopping_cart, size: 30, color: Colors.red),
+            ),
+            IconButton(
+              onPressed: () {
+                context.read<SignInBloc>().add(SignOutRequired());
+              },
+              icon: const Icon(Icons.logout_outlined),
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_cart, size: 30, color: Colors.red),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 9 / 16),
+              itemCount: 4,
+              itemBuilder: (context, int i) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green,
+                  ),
+                );
+              }),
+        ),
+        bottomNavigationBar: Container(
+          child: GNav(
+            color: Colors.grey,
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: () {
-              context.read<SignInBloc>().add(SignOutRequired());
-            },
-            icon: const Icon(Icons.logout_outlined),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 9 / 16),
-            itemCount: 4,
-            itemBuilder: (context, int i) {
-              return Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.green,
-                ),
-              );
-            }),
-      ),
-    );
+        ));
   }
 }
