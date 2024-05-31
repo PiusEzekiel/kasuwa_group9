78b265147ec5e15d8106a726812b56b4d3903fbd
Caro <carolinegyireh@gmail.com>
2024-05-31 21:52:55 +0200

    home screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
